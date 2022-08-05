8c9f367a9d19595c8b7e64219540b2604a3954bd
IvanBedoya <ivan.bedoya.dbs@gmail.com>
2022-08-05 11:57:55 -0500

    Agregar inicializacion de spring y codigo de Mysql

---
+++ crud/src/test/java/monolito/example/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package monolito.example.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
